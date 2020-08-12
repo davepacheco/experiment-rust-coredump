1cf816efa796f1fb0b1f37ae077b7c8596dc4099
David Pacheco <dap@oxidecomputer.com>
2020-08-12 12:35:52 -0700

    initial commit (panic=abort)

---
+++ run_all.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#
+# run_all.sh: runs the included binary using all configurations of:
+#
+#     (panicstrategy, use coredump crate, try to catch the panic)
+#
+# The panic strategy is specified by CARGO_PROFILE_DEV_PANIC=abort.  The other
+# two options are determined by command-line flags to the binary.
+#
+
+PANIC_STRATEGIES="unwind abort"
+USE_COREDUMP_CRATE="false true"
+TRY_CATCH="false true"
+
+echo "uname:     $(uname -a)"
+echo "cargo:     $(cargo --version)"
+echo "ulimit -c: $(ulimit -c)"
+
+for strategy in $PANIC_STRATEGIES; do
+	for use_crate in $USE_COREDUMP_CRATE; do
+		for try_catch in $TRY_CATCH; do
+			echo "------------------------------------"
+			echo "panic strategy:     $strategy"
+			echo "use coredump crate: $use_crate"
+			echo "try to catch panic: $try_catch"
+			(set -o xtrace;
+			RUST_BACKTRACE=full \
+			CARGO_PROFILE_DEV_PANIC=$strategy \
+			cargo run -- $use_crate $try_catch
+			echo "bash exit status = $?" )
+			echo
+		done
+	done
+done
